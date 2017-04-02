f72dd1701b49034da40d90dc5756ea0dbcf83433
Henry Till <henrytill@gmail.com>
2017-04-02 19:03:31 -0400

    Xcat_util: add {compose,flip}

--- src/xcat_util.ml
+++ src/xcat_util.ml
@@ -1,3 +1,7 @@
 let id x = x
 
 let const x _ = x
+
+let compose f g = fun x -> f (g x)
+
+let flip f x y = f y x
--- src/xcat_util.mli
+++ src/xcat_util.mli
@@ -1,3 +1,7 @@
 val id : 'a -> 'a
 
 val const : 'a -> ('b -> 'a)
+
+val compose : ('b -> 'c) -> ('a -> 'b) -> 'a -> 'c
+
+val flip : ('a -> 'b -> 'c) -> 'b -> 'a -> 'c
